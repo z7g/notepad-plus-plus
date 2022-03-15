7dc8eb8cd0c7a2e99b9cadab397aea99d9bb77e1
LAPTOP-GR8M022D\zhaoqijie <zhaoqijie@qdspr.com>
2022-03-15 12:25:02 +0800

    change createMutex parameter from nppInstance to My_nppInstance

--- PowerEditor/src/winmain.cpp
+++ PowerEditor/src/winmain.cpp
@@ -416,7 +416,7 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int)
 
 	bool TheFirstOne = true;
 	::SetLastError(NO_ERROR);
-	::CreateMutex(NULL, false, TEXT("nppInstance"));
+	::CreateMutex(NULL, false, TEXT("My_nppInstance"));
 	if (::GetLastError() == ERROR_ALREADY_EXISTS)
 		TheFirstOne = false;
 
